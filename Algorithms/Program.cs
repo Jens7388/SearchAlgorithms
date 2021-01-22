d35a8c412ced54063defd1ad5e6967a612831234
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2021-01-22 10:50:52 +0100

    program now checks if there are more than 1 of the number that is searched for

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Algorithms
 {
@@ -6,8 +8,18 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            int[] array = { 1, 2, 3, 4, 3, 5, 6 };
-            BinarySearch(array, 7);
+            int[] array = new int[10];
+            for(int i = 0; i < 4; i++)
+            {
+                array[i] = 2;
+            }
+            for(int i = 4; i < array.Length; i++)
+            {
+                array[i] = 3;
+            }
+
+            BinarySearch(array, 2);
+            LinearSearch(array, 2);
         }
 
         private static int BinarySearch(int[] array, int x)
@@ -15,7 +27,10 @@ namespace Algorithms
             int left = array[0];
             int right = array[array.Length - 1];
             int mid;
-            if(IsSorted(array)){
+            if(IsSorted(array))
+            {
+                Stopwatch timer = new Stopwatch();
+                timer.Start();
                 while(left < right)
                 {
                     mid = (left + right) / 2;
@@ -30,12 +45,40 @@ namespace Algorithms
                 }
                 if(array[left] == x)
                 {
-                    Console.WriteLine("Search successful!");
+                    int furthestLeft = left;
+                    int furthestRight = left;
+                    
+                    if(array[left - 1] == x)
+                    {
+                        while(left != 0 && array[left - 1] == x)
+                        {
+                            furthestLeft = left - 1;
+                             left--;
+                        }
+                    }
+                    else if(left != array.Length - 1 && array[left + 1] == x)
+                    {
+                        while(array[left + 1] == x)
+                        {
+                            furthestRight = left + 1;
+                            left++;
+                        }
+                    }
+                    //  Console.WriteLine("Search successful!");
+                    timer.Stop();
+
+                    timer.Reset();
+                    Console.WriteLine($"Desired number was found between indexes: {furthestLeft} and {furthestRight}");
+                    Console.WriteLine($"Binary search: {timer.ElapsedTicks}");
                     return left;
+
                 }
                 else
                 {
-                    Console.WriteLine("Array does not contain the desired number!");
+                    // Console.WriteLine("Array does not contain the desired number!");
+                    timer.Stop();
+                    Console.WriteLine($"Binary search: {timer.ElapsedTicks}");
+                    timer.Reset();
                     return -1;
                 }
             }
@@ -44,6 +87,28 @@ namespace Algorithms
                 Console.WriteLine("Error! Array is not sorted!");
                 return -1;
             }
+        }
+
+        private static int LinearSearch(int[] array, int x)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                if(x == array[i])
+                {
+                    //  Console.WriteLine("Search successful!");
+                    timer.Stop();
+                    Console.WriteLine($"Linear search: {timer.ElapsedTicks}");
+                    timer.Reset();
+                    return array[i];
+                }
+            }
+            //     Console.WriteLine("Array does not contain the desired number!");
+            timer.Stop();
+            Console.WriteLine($"Linear search: {timer.ElapsedTicks}");
+            timer.Reset();
+            return -1;
         }
 
         private static bool IsSorted(int[] array)
